4345f0caa290024a908bec1da1aeee9235c6809d
Grishchenko Timofei <timoxa810@gmail.com>
2018-09-14 11:55:45 +0300

    adding project

---
+++ git/epam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+
+3.       Есть возможность считать средний балл отдельного студента
+4.       Есть возможность считать средний балл всей группы
+
+*/
+namespace epam
+{
+    public class Group
+    {
+        public int GroupID { get; set; }
+    }
+    public class Student:Group
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public List<int> Rating;
+    }
+
+
+    class Program
+    {
+        public static List<Student> students = new List<Student>
+            {
+                new Student {First="Лидия", Last="Иванова", GroupID = 1, Rating= new List<int> {4, 5, 6, 7}},
+                new Student {First="Светлана", Last="Крыж", GroupID = 1, Rating= new List<int> {9, 8, 9, 7}},
+                new Student {First="Дмитрий", Last="Белько", GroupID = 2, Rating= new List<int> {5, 5, 5, 5, 5}}
+            };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В какой группе желаете посчитать средний балл?");
+            int id = Int32.Parse(Console.ReadLine());
+
+            var studentQuery1 =
+                from student in students
+                let totalScore = student.Rating.Average()
+                where student.GroupID == id
+                select totalScore ;
+
+            Console.WriteLine("Средние балы по ученикам: ");
+            foreach (var s in studentQuery1)
+            {
+                Console.WriteLine(s);
+            }
+
+            double averageScore = studentQuery1.Average();
+            Console.WriteLine("Средний балл группы = {0}", averageScore);
+            Console.ReadLine();
+        } 
+    }
+}
