48889d04f781c1ae3fb81f323b29398278028b59
Grishchenko Timofei <timoxa810@gmail.com>
2018-10-01 15:56:55 +0300

    unittest

---
+++ UnitTest/UnitTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class Triangle
+    {
+        public bool IsTriangle(double a, double b, double c)
+        {
+            return a + b > c && a + c > b && b + c > a && a >= 0 && b >= 0 && c >= 0;
+        }
+    }
+}
---
+++ UnitTest/UnitTestProject1/UnitTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestEnterSidesOfEgyptTriangle()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(true, triangle.IsTriangle(3, 4, 5));
+        }
+
+        [TestMethod]
+        public void TestEnterSameLengthforAllSides()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(true, triangle.IsTriangle(4, 4, 4));
+        }
+
+        [TestMethod]
+        public void TestEnterZeroLengthforAllSides()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void TestEnterNegativeLengthforAllSides()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(-3, -4, -5));
+        }
+
+        [TestMethod]
+        public void TestEnterrNegativeLengthforFirstSide()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(-3, 4, 5));
+        }
+
+        [TestMethod]
+        public void TestEnterTooLargeLengthForFirstSide()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(30, 3, 3));
+        }
+
+        [TestMethod]
+        public void TestEnterTooLargeLengthForSecondSide()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(1, 30, 1));
+        }
+
+        [TestMethod]
+        public void TestEnterTooLargeLengthForThirdSide()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(false, triangle.IsTriangle(1, 1, 30));
+        }
+
+        [TestMethod]
+        public void TestEnterFractialLengthofAllSides()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(true, triangle.IsTriangle(3.3, 4.4, 5.5));
+        }
+
+        [TestMethod]
+        public void TestEnterBigLengthofSides()
+        {
+            var triangle = new Triangle();
+            Assert.AreEqual(true, triangle.IsTriangle(3000, 4000, 5000));
+        }
+    }
+}
