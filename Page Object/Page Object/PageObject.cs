06a3e657d50efdd8b91b7caa062a5485e8f476e1
Grishchenko Timofei <timoxa810@gmail.com>
2018-10-28 17:01:13 +0300

    entering same cities

---
+++ Page Object/Page Object/PageObject.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace Page_Object
+{
+    public class Enter_city
+    {
+        private IWebDriver driver;
+
+        By d_city = By.XPath("//input[@placeholder='From']");
+        By a_city = By.XPath("//input[@placeholder='To']");
+        By enter_result = By.XPath("//div[@class ='airport']");
+
+        public Enter_city(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Enter_dcity(string city)
+        {
+            driver.FindElement(d_city).Clear();
+            driver.FindElement(d_city).SendKeys(city);
+        }
+
+        public void Enter_acity(string city)
+        {
+            driver.FindElement(a_city).Clear();
+            driver.FindElement(a_city).SendKeys(city);
+        }
+
+        public void Enter_result_click()
+        {
+            driver.FindElement(enter_result).Click();
+        }
+    }
+
+    public class HomePage
+    {
+
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.airbaltic.com/en-BY/index");
+        }
+
+    }
+}
---
+++ Page Object/Page Object/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Page_Object
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        // Выбор одного города в качестве места отправления и прибытия
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            Enter_city enter = new Enter_city(driver);
+            HomePage homePage = new HomePage(driver);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+            
+            homePage.goToPage();
+
+            enter.Enter_dcity("RIX");
+            enter.Enter_result_click();
+
+            enter.Enter_acity("RIX");
+
+            Assert.AreEqual(driver.FindElement(By.ClassName("dropdown-item-empty")).Text,"Unfortunately, we do not fly to/from RIX");
+
+            driver.Close();
+            driver.Quit();
+
+        }
+    }
+}
