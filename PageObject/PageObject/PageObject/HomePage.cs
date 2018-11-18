3bfd49edca9fc547a6eaca8664f10a8f193aba7e
Grishchenko Timofei <timoxa810@gmail.com>
2018-11-18 21:23:01 +0300

    using nUnit

---
+++ PageObject/PageObject/PageObject/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject.PageObject
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.airbaltic.com/en-BY/index");
+        }
+    }
+}
---
+++ PageObject/PageObject/UnitTests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using PageObject.PageObject;
+
+namespace PageObject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            ChoosingCity enter = new ChoosingCity(driver);
+            HomePage homePage = new HomePage(driver);
+            homePage.goToPage();
+
+            enter.InputDepartureCity("RIX");
+            enter.Enter_result_click();
+            enter.Enter_acity("RIX");
+
+            Assert.AreEqual(driver.FindElement(By.ClassName("dropdown-item-empty")).Text, "Unfortunately, we do not fly to/from RIX");
+            driver.Quit();
+        }
+    }
+}
